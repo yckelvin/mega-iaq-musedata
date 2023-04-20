74cc9cae900eb17a10f207698b923b71cedb00f5
Kelvin Lee <yckelvin@users.noreply.github.com>
2023-04-20 16:53:40 +0800

    random mqtt user id

--- main.ts
+++ main.ts
@@ -1,44 +1,54 @@
 input.onButtonPressed(Button.A, function () {
     MuseOLED.clear()
-    MuseOLED.writeStringNewLine("The key is:")
     MuseOLED.writeStringNewLine(Key)
+    MuseOLED.writeStringNewLine(UserID)
 })
 input.onButtonPressed(Button.B, function () {
     MuseOLED.clear()
     basic.pause(1000)
-    MuseOLED.writeStringNewLine("Getting Temp value:")
-    basic.pause(1000)
-    MuseOLED.writeStringNewLine(MuseIoT.HKTIAQ(
+    value = MuseIoT.HKTIAQ(
     Key,
     "620a0a72b42bf200180ae085",
     "62739eeefad13d5e682f4b65",
     MuseIoT.deviceDescription.Temp,
     MuseIoT.methodDirection.NULL
-    ))
-    basic.pause(1000)
-    MuseOLED.writeStringNewLine("Getting Dust value:")
-    basic.pause(1000)
-    MuseOLED.writeStringNewLine(MuseIoT.HKTIAQ(
-    Key,
-    "620a0a72b42bf200180ae085",
-    "62739eeefad13d5e682f4b65",
-    MuseIoT.deviceDescription.Dust,
-    MuseIoT.methodDirection.NULL
-    ))
-    basic.pause(1000)
+    )
+    MuseOLED.writeStringNewLine("Temp: " + value)
+    if (value > "2000") {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        value = MuseIoT.HKTIAQ(
+        Key,
+        "620a0a72b42bf200180ae085",
+        "627395eefad13d5e6826c9d6",
+        MuseIoT.deviceDescription.NULL,
+        MuseIoT.methodDirection.switch_ON
+        )
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
 })
+let value = ""
 let Key = ""
-basic.showNumber(0)
+let UserID = ""
 MuseIoT.initializeWifi()
 basic.pause(5000)
-basic.showNumber(1)
 MuseIoT.setWifi("izowifi", "izo1234@")
 basic.pause(10000)
-basic.showNumber(2)
-MuseIoT.ConnectMuseDataMQTTbroker("KL234")
+UserID = "NAMS" + randint(0, 100)
+MuseIoT.ConnectMuseDataMQTTbroker(UserID)
 basic.pause(10000)
-basic.showNumber(3)
+MuseOLED.clear()
 Key = MuseIoT.GetTheSecurityKey("muselabs", "muselabs")
 MuseOLED.writeStringNewLine("Ready!")
-basic.pause(10000)
-basic.showIcon(IconNames.Yes)
